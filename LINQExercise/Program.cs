6029b9162d926a39cfdc7cf19423f60943aef9bd
Jon Frederick <jonFred151@gmail.com>
2023-12-28 16:14:50 -0600

    Completed

---
+++ LINQExercise/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+namespace LINQExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> VideoGame = new List<string>() { "Ni no Kuni", "Pokemon Violet", "Legend of Zelda Wind Waker" };
+
+            var orderedgames = VideoGame.OrderBy(x => x).ToList();
+
+            Console.WriteLine("Ordered List of Video Games by Length");
+            foreach (var game in orderedgames)
+            {
+                Console.WriteLine(game);
+            }
+
+
+
+
+
+        }
+    }
+}
